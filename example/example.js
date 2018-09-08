95e874d6bb63af98d2dfcee914ff5c82d3216c50
Ali Soueidan <ali.soueidan@outlook.de>
2018-09-08 16:24:12 +0200

    Change of wording

--- example/example.js
+++ example/example.js
@@ -9,15 +9,15 @@ setStargazer = {
   // Set the Tag name of the generated item (Div, Span, p, etc.)
   generateItemTag: "li",
   // Set classname which will title the generated Elements (also id -> classname-i will be generated)
-  generateItemClass: "backgroundstars",
+  generateItemClass: "stargazer-element",
   // Set the content which each element shell contain (could be Text, an Image, etc.)
   generateItemContent: "🍪",
   // Item-position can be setted up as like css positions (absolute, fixed, sticky, etc.)
   setPosition: "absolute",
-  // setBorderGap sets up the minimum gap to the edge of the container-element
+  // setBorderGap sets up the minimum gap to the edge of the container-element (negativ value for overflow)
   setBorderGap: 0,
   // Morphclass will be the classname of morphed Elements to set morph state of generated elements
-  setMorphClass: "blink",
+  setMorphClass: "action",
   // Morphspeed will set how fast the Morph will be executed in milliseconds
   setMorphSpeed: 1000,
   // Morphquantity will set how many Elements will morph in a MorpSpeed execution
