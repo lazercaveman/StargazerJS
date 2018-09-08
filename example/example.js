bdf0b19dda3638ef6ebac7c24120024ff9240981
Ali Soueidan <ali.soueidan@outlook.de>
2018-09-08 13:44:30 +0200

    Added generated elements content

--- example/example.js
+++ example/example.js
@@ -3,13 +3,15 @@
 
 setStargazer = {
   // Set quantity of genereted Elements
-    quantity: 300,
+    quantity: 50,
   // Set id or class of parent element which shell contain the generated Elemenets
-    selectContainer: "#container",
+    selectContainer: "body",
   // Set the Tag name of the generated item (Div, Span, p, etc.)
     generateItemTag: "span",
   // Set classname which will title the generated Elements (also id -> classname-i will be generated)
-    generateItemClass: "star",
+    generateItemClass: "backgroundstars",
+  // Set the content which each element shell contain (could be Text, an Image, etc.)
+    generateItemContent: "🍪",
   // Morphclass will be the classname of morphed Elements to set morph state of generated elements
     setMorphClass: "blink",
   // Morphspeed will set how fast the Morph will be executed in milliseconds
--- stargazer.js
+++ stargazer.js
@@ -1,88 +1,82 @@
-// Author: Ali Soueidan
-// Author URI: https//: www.alisoueidan.com
-
-//////
-// Positioning stars
-
 document.addEventListener("DOMContentLoaded", function() {
 
-  //////
-  // Fallback
-
-    if ( setStargazer["devMode"] != "off" ) {
+  //Activate Devmode to check if everything is setted up right - will be displayed in console
+  if ( setStargazer["devMode"] != "off" ) {
 
       console.log("😱 Devmode is activated, turn devMode off by editing the steStargazer object -> steStargazer{ devMode: \"off\" }")
 
       // Fallbackobject
-        fallbackObject = {
+      fallbackObject = {
           // Set quantity of genereted Elements
-            quantity: "NUMBER",
+          quantity: "NUMBER",
           // Set id or class of parent element which shell contain the generated Elemenets
-            selectContainer: "STRING",
+          selectContainer: "STRING",
           // Set the Tag name of the generated item (Div, Span, p, etc.)
-            generateItemTag: "STRING",
+          generateItemTag: "STRING",
           // Set classname which will title the generated Elements (also id -> classname-i will be generated)
-            generateItemClass: "STRING",
+          generateItemClass: "STRING",
+          // Set the content which each element shell contain (could be Text, an Image, etc.)
+          generateItemContent: "STRING",
           // Morphclass will be the classname of morphed Elements to set morph state of generated elements
-            setMorphClass: "STRING",
+          setMorphClass: "STRING",
           // Morphspeed will set how fast the Morph will be executed in milliseconds
-            setMorphSpeed: "NUMBER",
+          setMorphSpeed: "NUMBER",
           // Morphquantity will set how many Elements will morph in a MorpSpeed execution
-            setMorphQuantity: "NUMBER",
-        };
+          setMorphQuantity: "NUMBER",
+      };
 
       // Check if setStargazer object is defined
-        if ( typeof setStargazer == "undefined" ) {
+      if ( typeof setStargazer == "undefined" ) {
           setStargazer = {};
           console.log("💩 setStargazer | isn't defined! You have to set up the object -> steStargazer{}");
-        } else {
+      } else {
           console.log("✔️ setStargazerObject | is defined.")
-        }
+      }
 
       // Fallback-loop checking for error potentials
-        for (let fallbackProperty in fallbackObject){
+      for (let fallbackProperty in fallbackObject){
 
           if ( setStargazer[fallbackProperty] == null ) {
 
-            console.log("💩 " + fallbackProperty + "isn't setted up. steStargazer object -> " + 
+          console.log("💩 " + fallbackProperty + "isn't setted up. steStargazer object -> " + 
               fallbackObject[fallbackProperty]);
 
           } else {
-            console.log("✔️ " + fallbackProperty + " | is defined.")
+          console.log("✔️ " + fallbackProperty + " | is defined.")
           };
 
-        };
-        
-    };
+      };
+      
+  };
 
   //////
   // Execution
 
-    // Select star-divs
+  // Select star-divs
       const CONTAINER = document.querySelector(setStargazer["selectContainer"]);
 
-    // Set while loop for generating stars
+  // Set while loop for generating stars
       let quantityCounter = 0;
       while ( setStargazer["quantity"] >= quantityCounter ){
-        CONTAINER.insertAdjacentHTML('afterbegin', '<div class="' + setStargazer["generateItemClass"] + '" id="' + setStargazer["generateItemClass"] + '-' + quantityCounter +'"></div>');
-        ++quantityCounter;
+      CONTAINER.insertAdjacentHTML('afterbegin', '<div class="' + setStargazer["generateItemClass"] + '" id="' + setStargazer["generateItemClass"] + '-' + quantityCounter +'" style="position:absolute">' + setStargazer["generateItemContent"] + '</div>');
+      ++quantityCounter;
       };
 
-    // Selct generatedItems by class
+  // Selct generatedItems by class
       const GENERATEDITEM = document.querySelectorAll(setStargazer["selectContainer"] + " > ." + setStargazer["generateItemClass"]);
       
-    // Set counter  
+  // Set counter  
       let i = 0;
-    // Select every star and reposition it by coincidence
+  // Select every star and reposition it by coincidence
       GENERATEDITEM.forEach( function() {
-        // defining x coordinate
-          let x = Math.floor((Math.random() * 99) + 1);
-        // defining y coordinate
-          let y = Math.floor((Math.random() * 99) + 1);
-        // Setting star position x & Y
+      // defining x coordinate
+          let x = Math.floor((Math.random() * 95) + 1);
+      // defining y coordinate
+          let y = Math.floor((Math.random() * 95) + 1);
+      // Setting star position x & Y
           GENERATEDITEM[i].style.left = x + "%";
           GENERATEDITEM[i].style.top = y + "%";
-        // Counting up the counter
+      // Counting up the counter
           ++i;
       });
 
@@ -90,21 +84,21 @@ document.addEventListener("DOMContentLoaded", function() {
   //////
   // lets do some bling bling (super easy, super simple) 
 
-    // Setup interval timing
+  // Setup interval timing
       function blink(){
-        // Setup of a random selektor
+      // Setup of a random selektor
           let startID = Math.floor((Math.random() * 100) + 1);
-        // Selekting random star
+      // Selekting random star
           let selection = document.querySelector( "#" + setStargazer["generateItemClass"] + "-"+ startID);
-        // Adding blink-classs to selektion
+      // Adding blink-classs to selektion
           selection.classList.add(setStargazer["setMorphClass"]);
           setTimeout(function(){ 
-            // Removing Blink-class after timeout
+          // Removing Blink-class after timeout
               selection.classList.remove(setStargazer["setMorphClass"]);
           }, setStargazer["setMorphSpeed"]/2);
       };
 
-    // Let the magic beginn
+  // Let the magic beginn
       setInterval( blink, setStargazer["setMorphSpeed"]/setStargazer["setMorphQuantity"] );
 
 });
